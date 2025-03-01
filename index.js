3b88b12e9b992417583a04691ee4bb262c20bb40
Bas <55401336+BasToTheMax@users.noreply.github.com>
2025-03-01 12:54:00 +0000

    Work on file finder

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ const app = express();
 
 const port = env.port || 3000;
 
-let ext = ['html', 'php', 'js', 'lua', 'py'];
+let exts = ['html', 'php', 'js', 'lua', 'py'];
 let base = env.BASE  || __dirname + '/data'
 
 app.get('/', (req, res) => {
     res.redirect('/main');
 });
+
+app.get('/favicon.ico', (req, res) => res.status(404).send());
 
 app.use('/:user/*', handleTilde);
 app.use('/:user', handleTilde);
@@ -23,7 +25,37 @@ function handleTilde(req, res, next) {
         let params = req.params;
         let user = params.user;
         let path = params['0'] || 'index';
-        res.json({ user, path });
+
+        let fsPath = Path.join(base, user, path);
+        let fsAltPath = Path.join(base, user, path, 'index');
+
+        let info = Path.parse(path);
+        let ext = '';
+        if (info.ext == '') {
+            let paths = [];
+            for(let i = 0; i < exts.length; i++) {
+                paths.push(`${fsPath}.${exts[i]}`);
+                paths.push(`${fsAltPath}.${exts[i]}`);
+            }
+
+            console.log('findPath', findPaths(paths));  
+        } else {
+            log(`File has ext!`, info.ext);
+        }
+
+        res.json({ user, path, fsPath, fsAltPath, info });
+}
+
+function findPaths(paths) {
+    log(`Checking ${paths.length} paths!`);
+    for(let i = 0; i < paths.length; i++) {
+        let newPath = paths[i];
+        log(`Trying ${newPath}`);
+        if (fs.existsSync(newPath)) {
+            let info = fs.statSync(newPath);
+            log(`Found ${newPath}`, info);
+        }
+    }
 }
 
 app.listen(port, () => {
