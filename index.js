bca44326b2d7beba6585dbabf7bdf840928fbca2
Bas <55401336+BasToTheMax@users.noreply.github.com>
2025-03-02 15:29:07 +0000

    Add env vars and increase timeout

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 const env = process.env;
 const { log } = console;
 
-const e = require('express');
 const express = require('express');
 const fs = require('fs');
 const Path = require('path');
 const eol = require("eol")
 const { spawn } = require('child_process');
-const httpHeaders = require('http-headers')
+const httpHeaders = require('http-headers');
+const Url = require('url');
 const app = express();
 
 const port = env.port || 3000;
@@ -53,14 +53,36 @@ function handleTilde(req, res, next) {
             }
         }
 
+        let procEnv = {};
+        procEnv['CONTENT_LENGTH'] = req.body?.length;
+        procEnv['CONTENT_TYPE'] = req.header('content-type');
+        procEnv['GATEWAY_INTERFACE'] = 'CGI/1.1';
+        procEnv['PATH_INFO'] = path;
+        procEnv['PATH_TRANSLATED'] = filePath;
+
+        // Query params
+        var i = req.originalUrl.indexOf('?');
+        var query = req.originalUrl.substr(i+1);
+        console.log(query);
+        procEnv['QUERY_STRING'] = query;
+
+        procEnv['REMOTE_ADDR'] = '127.0.0.1';
+        procEnv['REMOTE_HOST'] = 'localhost';
+        procEnv['REQUEST_METHOD'] = req.method;
+        procEnv['SCRIPT_NAME'] = path;
+        procEnv['SERVER_NAME'] = '288255.xyz';
+        procEnv['SERVER_PORT'] = 80;
+        procEnv['SERVER_PROTOCOL'] = 'HTTP/1.0';
+        procEnv['SERVER_SOFTWARE'] = 'TILDE288255_CUSTOM/1.0'
+
         const proc = spawn(filePath, {
-            env: {},
+            env: procEnv,
             killSignal: 'SIGKILL'
         });
         let data = '';
         let start = Date.now();
         let isEnd = false;
-        let timelimit = 10;
+        let timelimit = 60 * 1000;
         
         setTimeout(() => {
             proc.kill('SIGKILL');
